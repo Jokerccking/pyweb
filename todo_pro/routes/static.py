d698fbe9bec0655466f6b04e76780ad5d7f3f553
Roy <2903329691@qq.com>
2017-03-29 13:47:01 +0800

    delete flaskpro todopro

--- todo_pro/routes/static.py
+++
@@ -1,9 +0,0 @@
-def route_static(request):
-    filename = request.query.get('file', 'doge.gif')
-    path = 'static/' + filename
-    body = b''
-    with open(path, 'rb') as f:
-        body += f.read()
-    header = b'HTTP/1.1 200 OK\r\nContent-Type: image/gif\r\n'
-    r = header + b'\r\n' + body
-    return r
